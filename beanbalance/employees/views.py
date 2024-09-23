e516ef4f1ad7f6380937390a5c5b796748305992
jeena5555 <jeena@turffmap.com>
2024-09-23 21:27:28 +0700

    test:views

--- beanbalance/employees/views.py
+++ beanbalance/employees/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, login
+from django.contrib import messages
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+
+    def get(self, request):
+        # code here
+        return render(request, 'login.html', {"form": None})
+
+    def post(self, request):
+        # code here
+        pass
+
+
+class LogoutView(View):
+
+    def get(self, request):
+        # code here
+        pass
